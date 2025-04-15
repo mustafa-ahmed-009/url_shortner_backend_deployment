8e5679317ef0daa8705ef80fbbecc4c51dbf3dc7
Mustafa <darsh0878@gmail.com>
2025-04-15 20:33:25 +0200

    secure to true

--- utils/setCookie.ts
+++ utils/setCookie.ts
@@ -4,7 +4,7 @@ export const setCookie = (res:Response , token:string)=>{
     const isProduction = process.env.NODE_ENV === "production"
     res.cookie("token" , token,{
         httpOnly : true , 
-        secure : isProduction , 
+        secure : true , 
         sameSite :"none" , 
         maxAge: 7*24*60*60*1000 , 
     })
